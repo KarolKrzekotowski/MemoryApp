c72d2aa6e81d2363c3012a3e6e84ca7a6e13322a
masterchlop <254661@student.pwr.edu.pl>
2023-03-24 20:32:56 +0100

    rankingi filtrowanie

--- app/src/main/java/com/example/memoryapp/ui/fragment/bottomSetup/BottomDialog.kt
+++ app/src/main/java/com/example/memoryapp/ui/fragment/bottomSetup/BottomDialog.kt
@@ -35,7 +35,7 @@ class BottomDialog : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        Log.i("duperka","hej")
+
 
         val buttonsList = mutableListOf<Button>()
         buttonsList.apply {
